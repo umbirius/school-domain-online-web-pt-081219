731fc753a5ba9180ebc41f0a7674736420116127
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-27 17:39:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,11 +2,13 @@
 
 class School 
   
+  ROSTER = {}
+  
   attr_reader :school
   
   def initialize (school)
     @school = school 
-    ROSTER = {}
+    
   end 
   
   def add_student (student, grade)
