61827f6b4055360e041ae4d1a1e06587cce71711
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-27 18:03:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,12 +19,14 @@ class School
     end
   end 
   
-  def grade=(grade)
-    @grade=grade
-  end 
   
-  def grade
-    @grade
-  end 
+ def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+    @roster[grade] = name.sort
+  end
   
 end 
